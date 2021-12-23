6597511854492796dcda4a65cca0202de4066697
Erik Debye <erik.debye@gmail.com>
2021-12-22 21:14:35 -0600

    Tried to find a fix to lack of flights Not working rn

--- javascript modules/scraper.js
+++ javascript modules/scraper.js
@@ -2,42 +2,47 @@
 const puppeteer = require('puppeteer');
 
 //import processing functions
-const {processFlightNums} = require('./processing');
-
+const { processFlightNums } = require('./processing');
 
 //Create class -- this is the one that will be pushed into supabase as jsonb
-class FlightsObj {
-  constructor(departurePort, arrivalPort, flight1, flight2, flight3, flight4, flight5, flight6){
+class FlightObj {
+  constructor(departurePort, arrivalPort, date, metadata) {
     this.departurePort = departurePort; //String of departure airport code
     this.arrivalPort = arrivalPort; //String of arrival airport code
-    this.flight1 = flight1; //an object containing the class below
-    this.flight2 = flight2;
-    this.flight3 = flight3;
-    this.flight4 = flight4;
-    this.flight5 = flight5;
-    this.flight6 = flight6;
+    this.date = date;
+    this.metadata = metadata;
   }
-};
+}
 
-//Create Metadata class 
-class Flight{
-  constructor(stops, planeChange, flightNums, deptTime, arrTime, duration, prices, seatsLeft){
+//Create Metadata class
+class MetadataObj {
+  constructor(stops, planeChange, flightNums, deptTime, arrTime, duration, prices, seatsLeft) {
     this.stops = stops; //number of stops or 'Nonstop' string
     this.planeChange = planeChange; // array with either a -1 or a 1 and an airport code. [-1] || [1, 'DAL']
     this.flightNums = flightNums; //array of flight numbers
-    this.deptTime = deptTime; //string formated like '5:42pm' according to 12-hr clock. 
+    this.deptTime = deptTime; //string formated like '5:42pm' according to 12-hr clock.
     this.arrTime = arrTime; //string formated like deptTime
     this.duration = duration; //String formatted like this: '8h 35m'
-    this.prices = prices //an array of prices -- formatted like this: [business, anytime[economyplus], wanna-get-away[economy]] where most expensive ticket is first.
+    this.prices = prices; //an array of prices -- formatted like this: [business, anytime[economyplus], wanna-get-away[economy]] where most expensive ticket is first.
     this.seatsLeft = seatsLeft; //number of seats left or -1
   }
-};
+}
 
 async function pageScrape(dept, arr, dateStr, url) {
   //initilize class variables
-  let departurePort = dept, arrivalPort = arr, date = dateStr, stops, planeChange, flightNums, deptTime, arrTime, duration, prices, seatsLife;
+  let departurePort = dept,
+    arrivalPort = arr,
+    date = dateStr,
+    stops,
+    planeChange,
+    flightNums,
+    deptTime,
+    arrTime,
+    duration,
+    prices,
+    seatsLife;
 
-  const browser = await puppeteer.launch({headless: false, defaultViewport: {width: 988, height: 977}});
+  const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] });
   const page = await browser.newPage();
   await page.goto(url);
   //Human Bahavior - Timeout for 2 secs
@@ -54,23 +59,53 @@ async function pageScrape(dept, arr, dateStr, url) {
   console.log(arrivalPort);
   console.log(date);
 
+  await page.waitForXPath('//*[@id="air-booking-product-0"]/div[6]/span/span/ul');
+  const rows = await page.$x('//*[@id="air-booking-product-0"]/div[6]/span/span/ul');
+
   //Scrape Flight data for row
-  for(let i = 1; i <= 6; i++){
+  for (let k = 1; k <= rows.length / 2 && rows.length / 2 > 0; k++) {
+    
     //Flight Nums
-    await page.waitForXPath(`//*[@id="air-booking-product-0"]/div[6]/span/span/ul/li[${i}]/div[1]/div/div/button/span[1]`);
-    const flightNumElement = await page.$x(`//*[@id="air-booking-product-0"]/div[6]/span/span/ul/li[${i}]/div[1]/div/div/button/span[1]`);
-    const flightNumStr = await page.evaluate(el => el.textContent, flightNumElement[0]);
+    await page.waitForXPath(
+      `//*[@id="air-booking-product-0"]/div[6]/span/span/ul/li[${i}]/div[1]/div/div/button/span[1]`
+    );
+    const flightNumElement = await page.$x(
+      `//*[@id="air-booking-product-0"]/div[6]/span/span/ul/li[${i}]/div[1]/div/div/button/span[1]`
+    );
+    const flightNumStr = await page.evaluate((el) => el.textContent, flightNumElement[0]);
     //Store final flightNums value
     flightNums = processFlightNums(flightNumStr);
-
-    //All row values
+   
     console.log(flightNums);
+    
+    //depttime
+    await page.waitForXPath(`//*[@id="air-booking-product-0"]/div[6]/span/span/ul/li[${i}]/div[2]/span/text()`);
+    const deptTimeElement = await page.$x(
+      `//*[@id="air-booking-product-0"]/div[6]/span/span/ul/li[${i}]/div[2]/span/text()`
+    );
+    const deptTimeStr = await page.evaluate((el) => el.textContent, deptTimeElement[0]);
+    const deptTimeAmPm = await page.$x(
+      `//*[@id="air-booking-product-0"]/div[6]/span/span/ul/li[${i}]/div[2]/span/span[2]`
+    );
+    const deptTimeAmPmStr = await page.evaluate((el) => el.textContent, deptTimeAmPm[0]);
+    //Store Final value
+    deptTime = `${deptTimeStr} ${deptTimeAmPmStr}`;
+    console.log(deptTime);
   }
-  browser.close();
+  //browser.close();
 }
+
+
+
+
+
+
+
+
+
 
 
 //export module source: https://youtu.be/nt9M-rlbWc8
 module.exports = {
   scraper: pageScrape,
-}
+};
